a46eb99e0c8d86f44f767ab1bb72324d99e4a536
codelikeagirl29 <miss_leading009@yahoo.com>
2023-04-20 13:53:29 -0400

    corrected use of baguetteBox

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,46 +6,49 @@
 // })
 
 // const gallery = baguetteBox.run(".gallery");
-// baguetteBox.show(index, gallery[0]);
 
-// const search = new Filter("search", "data-caption");
-// document.getElementById("search").addEventListener("keyup", filter);
+document.querySelector("#search").addEventListener("keyup", userSearch);
 
-// function filter() {
-// let input = document.getElementById("search");
-// let results = input.value.toLowerCase();
-// let img = document.getElementById('gallery');
-// let pic = img.getElementsByTagName('a');
+function userSearch() {
+   const caption = document.querySelectorAll("a[data-caption]");
+   let captionsList = [];
 
-// for (i = 0; i < a.length; i++) {
-//   a = pic[i].getElementsByTagName("a")[0];
-//   if (a.getAttribute("data-caption").toLowerCase().indexOf(results) > -1){
-//     a[i].style.display = ""
-//   } else {
-//     a[i].style.display = "none"
-//   }
-// }
-// }
-
-// if (input.value === "") {
-//   img.style.display = "";
-// }
-
-// filter();
-document.getElementById('search').addEventListener("keyup", filter)
-
-function filter(){
-    const searchFilter = document.getElementById('search');
-    const text = searchFilter.value.toLowerCase();
-    const images = document.getElementsByTagName('img');
-
-    for ( let i = 0; i< images.length; i++ ) {
-        const dataCaption = document.getElementsByTagName('a')[i].getAttribute('data-caption')
-    
-        if (dataCaption.toLowerCase().indexOf(text) > -1) {
-            images[i].style.display = '';
-        } else {
-            images[i].style.display = 'none';
-        }
-    }
+   for (let i = 0; i < caption.length; i++) {
+      let captions = caption[i].getAttribute("data-caption");
+      captionsList.push(captions.toLowerCase());
+      // create variable set it to user search input toLowerCase()
+      let searchVar = document.querySelector("#search");
+      searchVar = searchVar.value.toLowerCase();
+      // create conditional statement (if) to search indexOf
+      if (captionsList[i].indexOf(searchVar) > -1) {
+         // set style of lstCnt to display block
+         caption[i].style.display = "block";
+      } else {
+         // set style of lstCnt to display none
+         caption[i].style.display = "none";
+      }
+   }
 }
+
+console.log(userSearch());
+// document.getElementById('search').addEventListener('keyup', searchFilter);
+// let thumbnails = document.getElementsByTagName('img');
+
+// function searchFilter() {
+//     // grabs user input as its being typed into search field and converts it to all upper case for case sensitivity
+//     let userInput = document.getElementById('search').value.toUpperCase();
+//     // targets all a tags with the data-caption attribute
+//     let links = document.querySelectorAll('a[data-caption]');
+//     // iterating through all the a tags
+//     for (let i = 0; i < links.length; i++) {
+//         let captions = links[i].getAttribute('data-caption');
+//         //turning captions into upper case for case sensitivity
+//         captions = captions.toUpperCase();
+//         // check if the current value of the search input is included within any captions, if it is display the associated thumbnail, if not, hide it
+//         if (captions.includes(userInput)) {
+//             thumbnails[i].parentNode.style.display = '';
+//         } else {
+//             thumbnails[i].parentNode.style.display = 'none';
+//         }
+//     }
+// }
